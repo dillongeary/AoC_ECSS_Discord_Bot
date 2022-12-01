5ce2d8c82c36801ea76f6125b677801b73d37dac
Dillon <dgfg1g20@soton.ac.uk>
2022-12-01 20:45:27 +0000

    a

--- main.py
+++ main.py
@@ -1,15 +1,52 @@
-import os
-
 import discord
-from dotenv import load_dotenv
+import requests
+import json
 
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
+TOKEN = open(".token","r").read()
 
-client = discord.Client()
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+def generateLeaderboard():
+    URL = "https://adventofcode.com/2022/leaderboard/private/view/1667828.json"
+    headers = {"cookie":"session=53616c7465645f5f9ab8a65ad69679c5f98977388ba2e108cd099a58c3de60865065ff96f1dffb6f50da0dae3566537016c1454a62c41d08b068c2747bad98bf","Accept":'application/xhtml+xml'}
+    r = requests.get(URL,headers = headers)
+
+    aocdict = r.json()
+    memberInfo = aocdict["members"]
+
+    leaderboard = {}
+
+    for x in memberInfo:
+        personInfo = memberInfo[x]
+        name = personInfo["name"]
+        score = personInfo["local_score"]
+        if score != 0:
+            leaderboard.update({name:score})
+
+    leaderboard = {k: v for k, v in sorted(leaderboard.items(), key=lambda item: item[1], reverse=True)}
+
+    prettyPrint = "```\nRank | Score | Name\n-----|-------|---------------------\n"
+    i = 1
+
+    for name in leaderboard:
+        score = leaderboard[name]
+        string = "{:02d}".format(i) + ")  |  " + "{:03d}".format(score) + "  | " + name + "\n"
+        prettyPrint+= string
+        i+=1
+
+    prettyPrint+="```"
+
+    return prettyPrint
+
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    channel = client.get_channel(id=869302998453583916)
+    message = await channel.send(generateLeaderboard())
+    while True:
+        await asyncio.sleep(900)
+        await message.edit(content=generateLeaderboard())
 
 client.run(TOKEN)
